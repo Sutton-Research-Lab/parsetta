d98751c0765037c1a7ef1774f685e097749c8732
RealPolitiX <xrpatrick@gmail.com>
2021-02-16 06:47:26 -0600

    Update version number to 0.0.3

--- parsetta/__init__.py
+++ parsetta/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version_info__ = ('0', '0', '2')
+__version_info__ = ('0', '0', '3')
 __version__ = '.'.join(__version_info__)
 
-from . import parsing, utils
+from . import parsing, elements, utils
