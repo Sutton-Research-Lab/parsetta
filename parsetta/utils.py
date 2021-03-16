7afd2db1bda6054d7294d2cb75c14ee35cf4fa1f
RealPolitiX <xrpatrick@gmail.com>
2021-03-16 17:33:00 -0500

    Added file pickler

--- parsetta/utils.py
+++ parsetta/utils.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import json
+import cloudpickle as cpk
 
 
 def read_json(file, **kwds):
@@ -19,3 +20,19 @@ def write_json(dic, file, **kwds):
 
     with open(file, 'w') as f:
         json.dump(dic, f, **kwds)
+
+
+def pickle_file(obj, file):
+    """ Pickle object to file.
+    """
+
+    with open(file, 'wb') as f:
+        cpk.dump(obj, f)
+
+
+def load_pickle(file):
+    """ Load pickled file.
+    """
+
+    with open(file, 'rb') as f:
+        return cpk.load(f)
