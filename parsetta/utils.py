d02696ce3dfa429987d044042a4907c2ee02045e
RealPolitiX <xrpatrick@gmail.com>
2021-04-06 07:10:50 -0500

    Added findFiles to utils module

--- parsetta/utils.py
+++ parsetta/utils.py
@@ -4,6 +4,8 @@
 import numpy as np
 import json
 import cloudpickle as cpk
+import glob as g
+import natsort as nts
 
 
 def read_json(file, **kwds):
@@ -36,3 +38,23 @@ def load_pickle(file):
 
     with open(file, 'rb') as f:
         return cpk.load(f)
+
+
+def findFiles(fdir, fstring='', ftype='h5', **kwds):
+    """
+    Retrieve files named in a similar way from a folder.
+    
+    :Parameters:
+        fdir : str
+            Folder name where the files are stored.
+        fstring : str | ''
+            Extra string in the filename.
+        ftype : str | 'h5'
+            The type of files to retrieve.
+        **kwds : keyword arguments
+            Extra keywords for `natsorted()`.
+    """
+    
+    files = nts.natsorted(g.glob(fdir + fstring + '.' + ftype), **kwds)
+    
+    return files
